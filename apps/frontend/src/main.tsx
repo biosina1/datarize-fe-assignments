91e8ddb448f3afb8cd02f8ef6dfa01907338b45c
biosina1 <biosina1@naver.com>
2024-08-21 03:11:02 +0900

    feat: global scss 파일 적용

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './styles/global.scss'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
