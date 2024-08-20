87ea9be873c5ff5bb4f8c8b3d8b8fb9676823e9f
biosina1 <biosina1@naver.com>
2024-08-20 18:26:27 +0900

    chore: @tanstack/react-query 라이브러리 설치 및 적용

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
